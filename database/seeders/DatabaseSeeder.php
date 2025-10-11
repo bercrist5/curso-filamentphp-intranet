72ad0724391c6e82b41699cbcb3cd7069f6c994b
Cristian Bermeo <cristof1403@gmail.com>
2025-10-11 13:34:05 -0500

    se agregan paises, estados y ciudades con un paquete adicional

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+       /*  User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]); */
+        DB::table('users')->insert([
+            'name'=> 'administrador',
+            'email'=> 'admin@example.com',
+            'password'=>Hash::make('password')          
         ]);
+        $this->call(WorldTableSeeder::class);
     }
 }
